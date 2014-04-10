934b96f9e27237346f57d68dfd6fda664ebe30fc
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-04-10 14:54:03 +0200

    xyz2lmp.sh added

---
+++ src/xyz2lmp.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# creates final.xyz, converts it to lmp data file, corrects it and invokes
+# lammps for rdf calculation
+# 
+# syntax: $0 <dump.xyz> <log.lammps>
+
+# list of required software
+required="python atomsk tailxyz.sh perl"
+
+# show syntax and exit
+fail(){
+  echo "syntax: $0 <dump.xyz>" >&2
+  exit 1
+}
+
+# get the mass of an element
+getmass(){
+  python -c "import periodictable; print periodictable.$1._mass" && return
+  echo "Error: Element '$1'not in periodic table" >&2
+  echo "(or python error: import periodictable failed)" >&2
+  exit 1
+}
+
+# find and print missing programs
+missing=$(
+for prog in $required; do
+  type $prog >/dev/null 2>/dev/null || echo $prog
+done
+)
+if [ -n "$missing" ];then
+  echo "This script requires the following libraries:" >&2
+  echo -e "$missing" | sed 's/^/* /' >&2
+  echo
+  fail
+fi
+
+# validate input
+(( ${#@} == 1 )) || fail
+xyzfile="$1"
+[ -f "$xyzfile" ] || fail
+lmpfile=`basename "$xyzfile" .xyz`.lmp
+tmpfile=/tmp/pid$$_$RANDOM.final.xyz
+
+# extract last xyz dataset
+tailxyz.sh "$xyzfile" > $tmpfile
+
+# retrieve unique types in order
+types=`echo $(sed -n '3,$ s/^\s*\([A-Z][a-z]*\)\s.*$/\1/p' $tmpfile | perl -ne 'if (!defined $x{$_}) { print $_; $x{$_} = 1; }' )`
+
+rm "$lmpfile" 2>/dev/null
+atomsk $tmpfile "$lmpfile" | grep '.lmp'
+rm $tmpfile
+
+# add zero charge to every atom if required (might work with reaxff only)
+if [ "$(echo $atomstyle)" == "atom_style charge" ]; then
+  sed -i 's/\(\s*[0-9][0-9]*\s\s*[1-9][0-9]*\s\)\(\(\s*[-0-9.+e]*\)\{3\}\s*\)$/\1 0.0 \2 /' "$lmpfile"
+fi
+
+# add mass information to lammps data file
+masslines=$(
+  i=0
+  echo
+  echo "Masses"
+  echo
+  for type in $types; do
+    let i+=1
+    mass=`getmass $type`
+    echo "    $i    $mass"
+  done
+  echo
+)
+
+masslines="$(echo -e "$masslines" | sed 's/$/\\/')"
+
+sed -i "/^\s*Atoms/i $masslines
+" "$lmpfile"
+

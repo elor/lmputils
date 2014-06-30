3e17c3cf2623c4acfb05ee49c9572030c012c66a
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-06-30 16:51:45 +0200

    lmpio.cpp autoformatted

--- src/lmpio.cpp
+++ src/lmpio.cpp
@@ -23,23 +23,31 @@ using namespace LAMMPS_NS;
 #define SMALL_BUFFER_LENGTH 128
 #endif
 
-extern int lmpio_read(const char *filename, double **positions, int **types, double **masses, double **size, char **atom_style, int *numatoms, int *numtypes) {
+extern int lmpio_read(const char *filename, double **positions, int **types,
+  double **masses, double **size, char **atom_style, int *numatoms,
+  int *numtypes)
+{
 
-  if (numtypes == NULL){
-    if (masses != NULL) {
+  if (numtypes == NULL)
+  {
+    if (masses != NULL)
+    {
       return 1;
     }
   }
 
-  if (numatoms == NULL) {
-    if (positions != NULL || types !=NULL) {
+  if (numatoms == NULL)
+  {
+    if (positions != NULL || types != NULL)
+    {
       return 2;
     }
   }
 
   // let lmp2atom
   lmphandle lmp = lmp2atomstyle_create();
-  if (lmp2atomstyle_parse_file(lmp, filename) != 0) {
+  if (lmp2atomstyle_parse_file(lmp, filename) != 0)
+  {
     return 3;
   }
 
@@ -49,7 +57,8 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
 
   free(lmp);
 
-  if (ret != 0) {
+  if (ret != 0)
+  {
     //    cerr << "cannot read atom_style from lmp file " << filename << ". error #" << ret << endl;
     return 4;
   }
@@ -69,16 +78,15 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
 
   vector<string> tokens;
   istringstream iss(opts);
-  copy(istream_iterator<string>(iss),
-       istream_iterator<string>(),
-       back_inserter<vector<string> >(tokens)
-       );
+  copy(istream_iterator<string>(iss), istream_iterator<string>(),
+    back_inserter<vector<string> >(tokens));
 
   char ** argv = new char*[tokens.size()];
 
   //  cout << tokens.size() << endl;
 
-  for (int i = 0; i < tokens.size(); ++i) {
+  for (int i = 0; i < tokens.size(); ++i)
+  {
     argv[i] = strdup(tokens[i].c_str());
     //    cout << argv[i] << endl;
   }
@@ -86,9 +94,10 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
   int argc = tokens.size();
 
   LAMMPS lammps(argc, argv, MPI_COMM_WORLD);
-  for (int i = 0; i < tokens.size(); ++i) {
-      free(argv[i]);
-    }
+  for (int i = 0; i < tokens.size(); ++i)
+  {
+    free(argv[i]);
+  }
 
   delete[] argv;
 
@@ -105,44 +114,53 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
 
   //  cout << lammps.atom->natoms << " Atoms loaded" << endl;
 
-  if (lammps.atom->natoms == 0) {
+  if (lammps.atom->natoms == 0)
+  {
     return 5;
   }
 
   assert(lammps.atom->tag_enable != 0);
   assert(lammps.atom->tag_consecutive() != 0);
 
-  if (lammps.atom->natoms > MAXSMALLINT) {
+  if (lammps.atom->natoms > MAXSMALLINT)
+  {
     return 6;
   }
 
   // extract number of atoms
-  if (numatoms != NULL) {
+  if (numatoms != NULL)
+  {
     *numatoms = lammps.atom->natoms;
     assert(*numatoms == lammps.atom->nlocal);
   }
 
   // extract number of types
-  if (numtypes != NULL) {
-    // TODO verify
+  if (numtypes != NULL)
+  {
+    // verify number of atoms
+    assert(lammps.atom->ntypes > 0);
     *numtypes = lammps.atom->ntypes;
   }
 
   // extract atoms
-  if (positions != NULL) {
+  if (positions != NULL)
+  {
     double **x = lammps.atom->x;
     int *tag = lammps.atom->tag;
 
     // @user: don't forget to free()
-    *positions = reinterpret_cast<double*>(malloc((*numatoms) * sizeof(double) * 3));
+    *positions = reinterpret_cast<double*>(malloc(
+      (*numatoms) * sizeof(double) * 3));
 
     // copy flattened atom positions
     size_t id, idoffset;
-    for (size_t atom = 0; atom < *numatoms; ++atom, ++x) {
+    for (size_t atom = 0; atom < *numatoms; ++atom, ++x)
+    {
       id = tag[atom] - 1;
 
       idoffset = id * 3;
-      for (size_t coord = 0; coord < 3; ++coord) {
+      for (size_t coord = 0; coord < 3; ++coord)
+      {
         (*positions)[idoffset + coord] = (*x)[coord];
       }
     }
@@ -151,7 +169,8 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
   }
 
   // extract types
-  if (types != NULL) {
+  if (types != NULL)
+  {
     int *t = lammps.atom->type;
     int *tag = lammps.atom->tag;
 
@@ -159,7 +178,8 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
     *types = reinterpret_cast<int*>(malloc((*numatoms) * sizeof(int)));
 
     size_t id;
-    for (size_t atom = 0; atom < *numatoms; ++atom, ++t) {
+    for (size_t atom = 0; atom < *numatoms; ++atom, ++t)
+    {
       id = tag[atom] - 1;
 
       (*types)[id] = *t;
@@ -169,24 +189,28 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
   }
 
   // extract masses
-  if (masses != NULL) {
+  if (masses != NULL)
+  {
     double *mass = lammps.atom->mass;
 
     *masses = reinterpret_cast<double*>(malloc((*numtypes) * sizeof(double)));
-    
-    for (size_t type = 0; type < *numtypes; ++type) {
-      (*masses)[type] = mass[type+1];
+
+    for (size_t type = 0; type < *numtypes; ++type)
+    {
+      (*masses)[type] = mass[type + 1];
     }
 
     // done
   }
 
   // extract size
-  if (size != NULL) {
+  if (size != NULL)
+  {
     assert(lammps.domain->box_exist == 1);
     assert(lammps.domain->dimension == 3);
-    
-    if (lammps.domain->triclinic != 0) {
+
+    if (lammps.domain->triclinic != 0)
+    {
       return 7;
     }
 
@@ -205,8 +229,11 @@ extern int lmpio_read(const char *filename, double **positions, int **types, dou
 
   return 0;
 }
-  
-extern int lmpio_write(const char *filename, int numatoms, int numtypes, double *positions, int *types, double *masses, double *size, char **atom_style) {
+
+extern int lmpio_write(const char *filename, int numatoms, int numtypes,
+  double *positions, int *types, double *masses, double *size,
+  char **atom_style)
+{
   //  cerr << "lmpio_write not implemented yet" << endl;
   return -1;
 }

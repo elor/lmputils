bbdeb202008a158a7a606a26553f69a999eaa2ac
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-08-04 15:55:30 +0200

    lmp2rdf.sh: Doesn't need log.lammps and automatically calculates 10 Angstrom

--- scripts/lmp2rdf.sh
+++ scripts/lmp2rdf.sh
@@ -9,61 +9,55 @@
 # TODO: module load
 
 # list of required software
-required="lammps mass2element.py lmpcharges.sh"
+required="lammps mass2element.py lmpcharges.sh lmp2atomstyle"
 
 # show syntax and exit
 fail(){
-  echo "syntax: $0 <data.lmp> <log.lammps>" >&2
-  exit 1
+    echo "syntax: $0 <data.lmp>" >&2
+    exit 1
 }
 
 # find and print missing programs
 missing=$(
-for prog in $required; do
-  type $prog >/dev/null 2>/dev/null || echo $prog
-done
+    for prog in $required; do
+        type $prog >/dev/null 2>/dev/null || echo $prog
+    done
 )
 if [ -n "$missing" ];then
-  echo "This script requires the following libraries:" >&2
-  echo -e "$missing" | sed 's/^/* /' >&2
-  echo
-  fail
+    echo "This script requires the following libraries:" >&2
+    echo -e "$missing" | sed 's/^/* /' >&2
+    echo
+    fail
 fi
 
 # validate input
-(( ${#@} == 2 )) || fail
+(( ${#@} == 1 )) || fail
 lmpfile="$1"
 [ -f "$lmpfile" ] || fail
-logfile="$2"
-[ -f "$logfile" ] || fail
 
-# prepare directories
-mkdir -p rdf
-
-# retrieve LAMMPS parameters
-atomstyle=`grep atom_style "$logfile"`
-pairstyle=`grep -e '^\\s*pair_\\|fix qeq' "$logfile" | grep -v '#' | grep -v '\\$'`
+# get atom style by guessing from lmp file format
+atomstyle="atom_style `lmp2atomstyle "$lmpfile"`"
 
 # retrieve masses in order
 masses=`echo $(sed -n '/Masses/,/Atoms/ s/^\s*[1-9][0-9]*\s*//p' "$lmpfile")`
 
 # get types from masses
 types=$(
-  for mass in $masses; do
-    echo $(mass2element.py $mass) | cut -d ' ' -f 1
-  done
+    for mass in $masses; do
+        echo $(mass2element.py $mass) | cut -d ' ' -f 1
+    done
 )
 
 # concatenate types to unique pairs
 types_arr=($types)
 typepairs=$(
-  for i in `seq 1 ${#types_arr[@]}`; do
-    let tmpi=i-1
-    for j in `seq $i ${#types_arr[@]}`; do
-      let tmpj=j-1
-      echo "${types_arr[$tmpi]}${types_arr[$tmpj]}:$i $j"
+    for i in `seq 1 ${#types_arr[@]}`; do
+        let tmpi=i-1
+        for j in `seq $i ${#types_arr[@]}`; do
+            let tmpj=j-1
+            echo "${types_arr[$tmpi]}${types_arr[$tmpj]}:$i $j"
+        done
     done
-  done
 )
 
 # create lammps fixes
@@ -71,7 +65,7 @@ rdffixes=$(cat <<EOF
 $(
   if (( ${#types_arr[@]} > 1 )); then
     echo "compute rdfALL all rdf 400"
-    echo "fix ALL all ave/time 10 1 10 c_rdfALL file rdf/all.rdf mode vector"
+    echo "fix ALL all ave/time 10 1 10 c_rdfALL file all.rdf mode vector"
   fi
 )
 $(
@@ -80,7 +74,7 @@ $(
     types=`echo $pair | cut -d: -f 1`
     ids=`echo $pair | cut -d: -f 2`
     echo "compute rdf$types all rdf 400 $ids"
-    echo "fix $types all ave/time 10 1 10 c_rdf$types file rdf/$types.rdf mode vector"
+    echo "fix $types all ave/time 10 1 10 c_rdf$types file $types.rdf mode vector"
   done
 )
 EOF
@@ -89,10 +83,10 @@ EOF
 # preparing lmp copy
 
 if [ "$(echo $atomstyle)" == "atom_style charge" ]; then
-  tmpfile=/tmp/pid$$_$RANDOM.lmp
-  lmpcharges.sh "$lmpfile" > $tmpfile
+    tmpfile=/tmp/pid$$_$RANDOM.lmp
+    lmpcharges.sh "$lmpfile" > $tmpfile
 else
-  tmpfile="$lmpfile"
+    tmpfile="$lmpfile"
 fi
 
 echo "running lammps"
@@ -100,7 +94,9 @@ echo "running lammps"
 lammpscmds="
 $atomstyle
 read_data $tmpfile
-$pairstyle
+velocity all zero linear
+pair_style lj/cut 10
+pair_coeff * * 0 0
 $rdffixes
 run 1
 "
@@ -112,11 +108,8 @@ lammps -nocite -echo screen -log none -suffix opt << EOF
 $lammpscmds
 EOF
 
-echo
-
 (( $? != 0 )) && echo "lammps failed!" || echo "rdf calculations done"
 
 if [ "$tmpfile" != "$lmpfile" ]; then
-  rm $tmpfile
+    rm $tmpfile
 fi
-

349514338f44387800ccd8fd6989a79ed75c8afb
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-08-06 15:53:26 +0200

    xyz2lmp.sh: correcting box size by shrink-wrapping; using lmpsetmasses.sh to set masses

--- scripts/xyz2lmp.sh
+++ scripts/xyz2lmp.sh
@@ -7,20 +7,12 @@
 # note: use "--" instead of a file name to read from stdin
 
 # list of required software
-required="python atomsk perl"
+required="python atomsk perl lmpsizes.py lmpsetmasses.sh"
 
 # show syntax and exit
 fail(){
   echo "syntax: $0 <dump.xyz>" >&2
   echo "use '--' instead of a file name to read from stdin" >&2
-  exit 1
-}
-
-# get the mass of an element
-getmass(){
-  python -c "import periodictable; print periodictable.$1._mass" && return
-  echo "Error: Element '$1'not in periodic table" >&2
-  echo "(or python error: import periodictable failed)" >&2
   exit 1
 }
 
@@ -49,37 +41,18 @@ fi
 lmpfile=/tmp/pid$$_$RANDOM.lmp
 
 # retrieve unique types in order
-types=`sed -n '3,/^\s*[0-9]*\s*$/ s/^\s*\([A-Z][a-z]*\)\s.*$/\1/p' "$xyzfile" | perl -ne 'if (!defined $x{$_}) { print $_; $x{$_} = 1; }' | xargs`
+types=`sed -n '3,/^\s*[0-9]*\s*$/ s/^\s*\([A-Z][a-z]*\)\s.*$/\1/p' < "$xyzfile" | perl -ne 'if (!defined $x{$_}) { print $_; $x{$_} = 1; }' | xargs`
 
-rm "$lmpfile" 2>/dev/null
-atomsk $xyzfile "$lmpfile" >/dev/null
+# actual work
+atomsk $xyzfile $lmpfile >/dev/null || exit 1
+mv $lmpfile{,.raw}
+lmpsizes.py $lmpfile.raw s > $lmpfile || exit 1
+rm $lmpfile.raw
+lmpsetmasses.sh "$types" $lmpfile
 
 # cleanup of temporary files
 [ "$xyzisintmp" == "yes" ] && rm $xyzfile
 
-# add zero charge to every atom if required (might work with reaxff only)
-if [ "$(echo $atomstyle)" == "atom_style charge" ]; then
-  sed -i 's/\(\s*[0-9][0-9]*\s\s*[1-9][0-9]*\s\)\(\(\s*[-0-9.+e]*\)\{3\}\s*\)$/\1 0.0 \2 /' "$lmpfile"
-fi
-
-# add mass information to lammps data file
-masslines=$(
-  i=0
-  echo
-  echo "Masses"
-  echo
-  for type in $types; do
-    let i+=1
-    mass=`getmass $type`
-    echo "    $i    $mass"
-  done
-  echo
-)
-
-masslines="$(echo -e "$masslines" | sed 's/$/\\/')"
-
-sed "/^\s*Atoms/i $masslines
-" "$lmpfile"
-
-rm "$lmpfile"
+cat $lmpfile
+rm $lmpfile
 

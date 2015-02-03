b5671903b008a0743d39918e1e13d4bcc1b9497b
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-02-03 16:20:13 +0100

    LAMMPS: create_avec definition changed. Adopted the new argument types

--- src/lmpio.cpp
+++ src/lmpio.cpp
@@ -102,7 +102,9 @@ extern int lmpio_read(const char *filename, double **positions, int **types,
   delete[] argv;
 
   // atom_style
-  lammps.atom->create_avec(style, 0, NULL, lammps.suffix);
+  // AAaaand the LAMMPS team changed the
+//  lammps.atom->create_avec(style, 0, NULL, lammps.suffix);
+  lammps.atom->create_avec(style, 0, NULL, 1);
 
   // read_data
   ReadData rd(&lammps);
